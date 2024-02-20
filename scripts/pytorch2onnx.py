e5ca25e4496010e7280bb6e2b8f63366be12613b
Shubham-jetson <shubhamgupto@gmail.com>
2024-02-20 14:23:59 -0500

    use argparse

--- scripts/pytorch2onnx.py
+++ scripts/pytorch2onnx.py
@@ -1,8 +1,12 @@
 from ultralytics import YOLO
+import argparse
 
+parser = argparse.ArgumentParser(description='Process pt file.')
+parser.add_argument('--pt_path', help='path to pt file', required=True)
+args = parser.parse_args()
 # TODO: Specify which model you want to convert
 # Model can be downloaded from https://github.com/ultralytics/ultralytics
-model = YOLO("../models/yolov8x.pt")
+model = YOLO(args.pt_path)
 model.fuse()
 model.info(verbose=False)  # Print model information
 model.export(format="onnx", simplify=True)
